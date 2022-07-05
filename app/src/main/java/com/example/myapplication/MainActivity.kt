af933e61cc9507e26f28a0e591b8ddfb5e81a08f
anreh <anurehka@gmail.com>
2022-07-05 11:06:20 +0800

    Commit 20220705 - new project commit - My Application

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.button)
+        button
+            .setOnClickListener { sayHi() }
     }
+
+    private fun sayHi() {
+        Toast.makeText(
+             this, "Hello Class!",
+            Toast.LENGTH_SHORT).show()
+    }
+
+
 }
