63b26365751c7d299af160d48b03096351962f53
anreh <anurehka@gmail.com>
2022-08-16 10:58:59 +0800

    W7 16/8/2022 -Chapter 3 data binding (View group and data group) -Add a layout for data binding -Add total score

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,16 +6,25 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
+import androidx.databinding.DataBindingUtil
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var diceImg:ImageView
     lateinit var numberText:TextView
     lateinit var editPlayerName:EditText
     lateinit var playerNameTV:TextView
+    lateinit var binding: ActivityMainBinding
+    private val gameInfo2: MyName= MyName("Tan Yogene")
+    private var totalScore :Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        //practical 7
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
 //practical 1
 //        val button: Button = findViewById(R.id.button)
 //        button
@@ -23,17 +32,22 @@ class MainActivity : AppCompatActivity() {
 
 
 //practical 2
-        diceImg = findViewById(R.id.diceImg)
-        numberText = findViewById(R.id.numbertext)
-        val rollButton: Button = findViewById(R.id.button)
+//        diceImg = findViewById(R.id.diceImg)
+//        numberText = findViewById(R.id.numbertext)
+//        val rollButton: Button = findViewById(R.id.button)
         //val rollButton = findViewById<Button>(R.id.button)
-        rollButton.setOnClickListener{ rollDice() }
+//        rollButton.setOnClickListener{ rollDice() }
 
 //practical 3
-        editPlayerName = findViewById(R.id.editPlayerName)
-        playerNameTV = findViewById(R.id.playername)
+//        editPlayerName = findViewById(R.id.editPlayerName)
+//        playerNameTV = findViewById(R.id.playername)
         val updateButton = findViewById<Button>(R.id.updatePlayerName)
-        updateButton.setOnClickListener{updatePlayerName(it)}
+//        updateButton.setOnClickListener{updatePlayerName(it)}
+
+//practical 7
+        binding.gameInfo =gameInfo2
+        binding.button.setOnClickListener{ rollDice() }
+        binding.updatePlayerName.setOnClickListener{updatePlayerName(it)}
     }
 
     //practical 1
@@ -44,11 +58,42 @@ class MainActivity : AppCompatActivity() {
     }
 
     //practical 2
+//    private fun rollDice(){
+//        val randomNumb = (1..6).random()
+////        val numberText:TextView = findViewById(R.id.numbertext)
+////        val diceImg: ImageView =findViewById(R.id.diceImg)
+//        numberText.text = randomNumb.toString()
+//        val imgSrc = when (randomNumb){
+//            1-> R.drawable.dice_1
+//            2-> R.drawable.dice_2
+//            3-> R.drawable.dice_3
+//            4-> R.drawable.dice_4
+//            5-> R.drawable.dice_5
+//            else-> R.drawable.dice_6        }
+//        diceImg.setImageResource(imgSrc)
+//
+//        Toast.makeText(this,randomNumb.toString(),Toast.LENGTH_SHORT).show() //small pop out
+//    }
+
+    //practical 3
+//    private fun updatePlayerName(view: View){
+//        playerNameTV.text = editPlayerName.text
+//
+//        editPlayerName.setText(" ")
+//        editPlayerName.clearFocus()
+//
+//        //Hide Keyboard after update player name
+//        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//            imm.hideSoftInputFromWindow(view.windowToken, 0)
+//
+//    }
+
+    //practical 7
     private fun rollDice(){
         val randomNumb = (1..6).random()
 //        val numberText:TextView = findViewById(R.id.numbertext)
 //        val diceImg: ImageView =findViewById(R.id.diceImg)
-        numberText.text = randomNumb.toString()
+       binding.numbertext.text = randomNumb.toString()
         val imgSrc = when (randomNumb){
             1-> R.drawable.dice_1
             2-> R.drawable.dice_2
@@ -56,27 +101,34 @@ class MainActivity : AppCompatActivity() {
             4-> R.drawable.dice_4
             5-> R.drawable.dice_5
             else-> R.drawable.dice_6        }
-        diceImg.setImageResource(imgSrc)
+        binding.diceImg.setImageResource(imgSrc)
+        totalScore+=randomNumb.toInt()
+        gameInfo2.totalScore="Total: " + totalScore.toString()
+        binding.gameInfo =gameInfo2
+
+
 
         Toast.makeText(this,randomNumb.toString(),Toast.LENGTH_SHORT).show() //small pop out
     }
-
-    //practical 3
-
-
     private fun updatePlayerName(view: View){
-        playerNameTV.text = editPlayerName.text
+//        binding.playername.text = binding.editPlayerName.text
 
-        editPlayerName.setText(" ")
-        editPlayerName.clearFocus()
+        //way 1
+//        gameInfo2.playerName=binding.editPlayerName.text.toString()
+//        binding.gameInfo =gameInfo2
+//       Ways 2(teachers)(this no need mention binding inside):
+        binding.apply{
+            gameInfo?.playerName = editPlayerName.text.toString()
+            invalidateAll()
+        }
+
+        binding.editPlayerName.setText(" ")
+        binding.editPlayerName.clearFocus()
 
         //Hide Keyboard after update player name
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
 
     }
-
-
-
 
 }
---
+++ app/src/main/java/com/example/myapplication/MyName.kt
@@ -0,0 +1,3 @@
+package com.example.myapplication
+
+data class MyName(var playerName: String="", var totalScore: String="")
